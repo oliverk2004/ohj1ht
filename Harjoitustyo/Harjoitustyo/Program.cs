fc4012069429a87a3dd1ebe0c1d3ddc49b9cf9f8
oliverk2004 <oliver.kanden04@gmail.com>
2026-02-26 20:50:09 +0200

    työ aloitettu. pohja pelille luotu

---
+++ Harjoitustyo/Harjoitustyo/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Jypeli;
+using Timer = Jypeli.Timer;
+
+namespace Harjoitustyo
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            new FysiikkaTetrisGame().Run();
+        }
+    }
+
+    public class FysiikkaTetrisGame : PhysicsGame
+    {
+        public override void Begin()
+        {
+            Level.Background.Color = Color.Black;
+            Camera.ZoomToLevel();
+            Gravity = new Vector(0, -800);
+            
+            Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+
+            MessageDisplay.Add("Fysiikka‑Tetris");
+        }
+    }
+}
